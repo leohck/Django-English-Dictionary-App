9e4b8d494b85a26afcb322c83af6ef90198fee78
Leonardo Black <leonardoblack.net@gmail.com>
2019-08-09 14:52:50 -0300

    This publishing will alter the css and will implement the english dictionary funciton

---
+++ EnglishDictionaryAPP/main.py
@@ -0,0 +1,42 @@
+#  Copyright (c)  8/8/2019
+#  Developed by: Leonardo Black | 21 years old | Computer Engineer
+
+
+import os
+import json
+from difflib import get_close_matches
+
+
+with open(os.path.join("EnglishDictionaryAPP", "data", "data.json"), 'r') as jsonfile:
+    data = json.load(jsonfile)
+
+
+def translate(word_to_translate):
+    word_to_translate = word_to_translate.lower()
+    matches = get_close_matches(word_to_translate, data.keys())
+    if word_to_translate in data:
+        return data[word_to_translate]
+    elif word_to_translate.capitalize() in data:
+        return data[word_to_translate.capitalize()]
+    elif word_to_translate.title() in data:
+        return data[word_to_translate.title()]
+    elif word_to_translate.upper() in data:
+        return data[word_to_translate.upper()]
+    elif len(matches) > 0:
+        return data[matches[0]]
+    else:
+        return "The word %s doesn't exist or not found!" % word_to_translate.upper()
+
+
+if __name__ == '__main__':
+    while True:
+        word = input("Enter a word to search for <>: ")
+        if word == "\\end":
+            break
+        else:
+            responses = translate(word)
+            if type(responses) == list:
+                for response in responses:
+                    print(response)
+            else:
+                print(responses)
--- EnglishDictionaryAPP/views.py
+++ EnglishDictionaryAPP/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import MeaniningForm
+from .main import translate
+
 
 # Create your views here.
+def home(request):
+    meaningform = MeaniningForm(request.POST or None)
+    context = {'meaningform': meaningform}
+    if meaningform.is_valid():
+        print(meaningform.cleaned_data)
+        response = meaningform.cleaned_data['word']
+        context['response'] = translate(response)
+        return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context=context)
